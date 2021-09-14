615240ef02183d5f4c8930dfe73094771d96971b
xys <1343025166@qq.com>
2021-09-14 18:05:31 +0800

    add 依赖库

---
+++ app/src/main/java/com/xysss/keeplearning/app/App.kt
@@ -0,0 +1,19 @@
+package com.xysss.keeplearning.app
+
+import android.app.Application
+
+/**
+ * Author:bysd-2
+ * Time:2021/9/1415:02
+ */
+
+class App: Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+
+
+}
